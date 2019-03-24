12f9b50bc42776f79b3d8dd94ff68412dc650f9f
Harrie Hazewinkel <hhazewinkel@gmail.com>
2019-03-24 16:27:27 +0100

    Merge method specific tests in method specific class.

---
+++ _integration-test/src/test/java/net/lisanza/dropunit/integrationtest/GetTestIT.java
@@ -0,0 +1,93 @@
+package net.lisanza.dropunit.integrationtest;
+
+import net.lisanza.dropunit.client.ClientDropUnit;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.net.SocketTimeoutException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class GetTestIT extends BaseRequest {
+
+    private static final String RESPONSE_FILE = "src/test/resources/xml/drop-response.xml";
+
+    @Test
+    public void shouldTestWithPath() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withGet("test-get/with/path")
+                .withResponseOkFromFile(MediaType.APPLICATION_XML, RESPONSE_FILE)
+                .drop();
+
+        HttpResponse response = httpClient.executeBasicHttpGet(dropUnit.getUrl());
+        assertEquals(200, response.getStatusLine().getStatusCode());
+
+        String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+        assertNotNull(body);
+        assertThat(body, containsString(dropUnit.getResponseBody()));
+
+        dropUnit.assertCount(1);
+    }
+
+    @Test
+    public void shouldTestWithQueryString() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withGet("test-get/with/path?and=variables")
+                .withResponseOkFromFile(MediaType.APPLICATION_XML, RESPONSE_FILE)
+                .drop();
+
+        HttpResponse response = httpClient.executeBasicHttpGet(dropUnit.getUrl());
+        assertEquals(200, response.getStatusLine().getStatusCode());
+
+        String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+        assertNotNull(body);
+        assertThat(body, containsString(dropUnit.getResponseBody()));
+
+        dropUnit.assertCount(1);
+    }
+
+    @Test
+    public void shouldTestWithException() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withGet("test-get-exception")
+                .withResponseBadRequest(MediaType.APPLICATION_XML, "")
+                .drop();
+
+        HttpResponse response = httpClient.executeBasicHttpGet(dropUnit.getUrl());
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatusLine().getStatusCode());
+
+        dropUnit.assertCount(1);
+    }
+
+    @Test
+    public void shouldTestWithConnectionTimeout() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withGet("test-get")
+                .withResponseOkFromFile(MediaType.APPLICATION_XML, RESPONSE_FILE)
+                .withResponseDelay(20000)
+                .drop();
+
+        try {
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectionRequestTimeout(1000)
+                    .setConnectTimeout(1000)
+                    .setSocketTimeout(1000)
+                    .build();
+            httpClient.executeBasicHttpGet(dropUnit.getUrl(), requestConfig);
+            assertTrue(false);
+        } catch (SocketTimeoutException e) {
+            assertTrue(true);
+        }
+
+        dropUnit.assertCount(1);
+    }
+}
---
+++ _integration-test/src/test/java/net/lisanza/dropunit/integrationtest/PutTestIT.java
@@ -0,0 +1,100 @@
+package net.lisanza.dropunit.integrationtest;
+
+import net.lisanza.dropunit.client.ClientDropUnit;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.net.SocketTimeoutException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class PutTestIT extends BaseRequest {
+
+    private static final String REQUEST_FILE = "src/test/resources/xml/drop-request.xml";
+    private static final String RESPONSE_FILE = "src/test/resources/xml/drop-response.xml";
+
+    @Test
+    public void shouldTestWithPath() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withPut("test-put/with/path")
+                .withRequestBodyFromFile(MediaType.APPLICATION_XML, REQUEST_FILE)
+                .withResponseOkFromFile(MediaType.APPLICATION_XML, RESPONSE_FILE)
+                .drop();
+
+        HttpResponse response = httpClient.executeBasicHttpPut(dropUnit.getUrl(), REQUEST_FILE, MediaType.APPLICATION_XML);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+
+        String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+        assertNotNull(body);
+        assertThat(body, containsString(dropUnit.getResponseBody()));
+
+        dropUnit.assertCount(1);
+    }
+
+    @Test
+    public void shouldTestWithQueryString() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withPut("test-put")
+                .withRequestBodyFromFile(MediaType.APPLICATION_XML, REQUEST_FILE)
+                .withResponseOkFromFile(MediaType.APPLICATION_XML, RESPONSE_FILE)
+                .drop();
+
+        HttpResponse response = httpClient.executeBasicHttpPut(dropUnit.getUrl(), REQUEST_FILE, MediaType.APPLICATION_XML);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+
+        String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+        assertNotNull(body);
+        assertThat(body, containsString(dropUnit.getResponseBody()));
+
+        dropUnit.assertCount(1);
+    }
+
+    @Test
+    public void shouldTestWithException() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withPut("test-put-exception")
+                .withRequestBodyFromFile(MediaType.APPLICATION_XML, REQUEST_FILE)
+                .withResponseBadRequest(MediaType.APPLICATION_XML, "")
+                .drop();
+
+        HttpResponse response = httpClient.executeBasicHttpPut(dropUnit.getUrl(), REQUEST_FILE, MediaType.APPLICATION_XML);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatusLine().getStatusCode());
+
+        dropUnit.assertCount(1);
+    }
+
+    @Test
+    public void shouldTestWithConnectionTimeout() throws Exception {
+        ClientDropUnit dropUnit = new ClientDropUnit(DROP_UNIT_HOST)
+                .withPut("test-put")
+                .withRequestBodyFromFile(MediaType.APPLICATION_XML, REQUEST_FILE)
+                .withResponseOkFromFile(MediaType.APPLICATION_XML, RESPONSE_FILE)
+                .withResponseDelay(20000)
+                .drop();
+
+        try {
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectionRequestTimeout(1000)
+                    .setConnectTimeout(1000)
+                    .setSocketTimeout(1000)
+                    .build();
+            httpClient.executeBasicHttpPut(dropUnit.getUrl(),
+                    REQUEST_FILE, MediaType.APPLICATION_XML,
+                    requestConfig);
+            assertTrue(false);
+        } catch (SocketTimeoutException e) {
+            assertTrue(true);
+        }
+
+        dropUnit.assertCount(1);
+    }
+}
